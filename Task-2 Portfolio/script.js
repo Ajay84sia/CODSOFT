88ab485d8d821da25dff55bf0a60d9b8c1fb7b7d
Ajay84sia <ajay9679339@gmail.com>
2023-07-15 17:02:15 +0530

    navbar

---
+++ Task-2 Portfolio/script.js
@@ -0,0 +1,17 @@
+var sidemenu = document.getElementById("sidemenu");
+
+
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+
+
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
+
+
+
+function homePage(){
+  window.location.href='#home'
+}
